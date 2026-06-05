a0a9b0286a68703c5e764b2fadeb8b289a0cb9f6
Alfredo20207 <alfredoresendiz39@gmail.com>
2026-06-04 19:02:15 -0600

    Mapa interactivo
    Mapa interactivo de Queretaro, basico

--- mapa!.py
+++ mapa!.py
@@ -1,7 +1,12 @@
 import folium
 
-# Centrar el mapa en Querétaro
-mapa = folium.Map(location=[20.588, -100.388], zoom_start=13)
-folium.Marker([20.588, -100.388], popup="Centro de Querétaro").add_to(mapa)
+# Centrar el mapa en Querétaro (Vista Satelital)
+mapa = folium.Map(
+    location=[20.477694736471342, -100.12033044415305], 
+    zoom_start=13,
+    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attr='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+)
+
 
 mapa.save("mapa_queretaro.html")
