dde013a4f3b6477a545c82f731a1f94ed8212535
Alfredo20207 <alfredoresendiz39@gmail.com>
2026-06-15 00:28:20 -0600

    consults
    Se crea archivo para consultas basicas

---
+++ consults.sql
@@ -0,0 +1,12 @@
+-- ritmo de carrera antes y despues del pitstop
+
+SELECT
+	r.season as temporada,
+	ra.name as gran_premio,
+	d.surname as piloto,
+	r.round as ronda,
+	MIN(r.fastestLapTime) as mejor_vuelta
+from results r
+join races ra on r.season = ra.season and r.round = ra.round
+join drivers d on r.driverId = d.driverId
+group by r.season, ra.name, d.surname, r.round;
